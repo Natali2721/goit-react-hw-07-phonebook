2545c0df0b8b06d174af0a5c549175023e652b19
Yukhnenko Nataliia <101944838+Natali2721@users.noreply.github.com>
2022-12-18 17:23:16 +0200

    Update contactsApi.js

--- src/redux/contactsApi.js
+++ src/redux/contactsApi.js
@@ -3,17 +3,17 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const contactsApi = createApi({
   reducerPath: 'contacts',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://639cb5e116d1763ab152294c.mockapi.io/contacts/',
+    baseUrl: 'https://639cb5e116d1763ab152294c.mockapi.io/contacts',
   }),
   tagTypes: ['Contact'],
   endpoints: builder => ({
     getContacts: builder.query({
-      query: () => `contacts`,
+      query: () => `/contacts`,
       providesTags: ['Phonebook'],
     }),
     addContact: builder.mutation({
       query: contact => ({
-        url: 'contacts/',
+        url: '/contacts',
         method: 'POST',
         body: contact,
       }),
@@ -21,7 +21,7 @@ export const contactsApi = createApi({
     }),
     deleteContact: builder.mutation({
       query: id => ({
-        url: `contacts/${id}`,
+        url: `/contacts/${id}`,
         method: 'DELETE',
       }),
       invalidatesTags: ['Phonebook'],
