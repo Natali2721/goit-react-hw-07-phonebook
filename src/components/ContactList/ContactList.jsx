41db7178144606b249d7c46a72cd486e08cc1e3d
Yukhnenko Nataliia <101944838+Natali2721@users.noreply.github.com>
2022-12-18 16:50:12 +0200

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -5,48 +5,48 @@ import {
   ContactTxt,
 } from 'components/Style/Element.styled';
 import { FaUserAlt } from 'react-icons/fa';
-import { deleteContact } from 'redux/phonebookSlice';
-import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
+import {
+  useDeleteContactMutation,
+  useGetContactsQuery,
+} from '../../redux/contactsApi';
 
 export const ContactList = () => {
-  const dispatch = useDispatch();
-  const contacts = useSelector(state => state.contacts);
-
-  const deleteContactById = contactId => {
-    dispatch(deleteContact(contactId));
-  };
-
+  const { data } = useGetContactsQuery();
   const searchFilter = useSelector(state => state.filter);
-
   const serchFilterToLowerCase = searchFilter.toLowerCase();
+
+  const deleteContact = useDeleteContactMutation();
 
   const getVisibleContacts = () => {
     if (searchFilter !== '') {
-      return contacts.filter(({ userName }) =>
+      return data.filter(({ userName }) =>
         userName.toLowerCase().includes(serchFilterToLowerCase)
       );
     }
-    return contacts;
+    return data;
   };
 
   const visibleContacts = getVisibleContacts();
 
+  console.log(visibleContacts);
+
   return (
     <Contacts>
-      {visibleContacts.map(({ userName, number, id }) => {
-        return (
-          <ContactItem key={id}>
-            <FaUserAlt />
-            <ContactTxt>
-              {userName} : {number}
-            </ContactTxt>
-            <Button type="button" onClick={() => deleteContactById(id)}>
-              Delete
-            </Button>
-          </ContactItem>
-        );
-      })}
+      {visibleContacts &&
+        visibleContacts.map(contact => {
+          return (
+            <ContactItem key={contact.id}>
+              <FaUserAlt />
+              <ContactTxt>
+                {contact.userName} : {contact.number}
+              </ContactTxt>
+              <Button type="button" onClick={() => deleteContact(contact.id)}>
+                Delete
+              </Button>
+            </ContactItem>
+          );
+        })}
     </Contacts>
   );
 };
